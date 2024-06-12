c0cc8297fcd5a703929b2f09a12c41f8c6687d94
MalladiMadhu129 <113227404+MalladiMadhu129@users.noreply.github.com>
2024-06-12 10:01:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './public'));
 });
 
 app.use('*', (req, res) => {
